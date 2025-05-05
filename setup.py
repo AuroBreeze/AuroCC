51e1c209d5ef328c31b61eedd81d5880b4979b3b
AuroBreeze <1732373074@qq.com>
2025-05-05 21:08:10 +0800

    build(AuroCC): 更新项目构建配置
    - 移除 MANIFEST.in 文件，简化构建过程
    - 更新 requirements.txt，添加 setuptools 依赖
    - 修改 setup.py:
      - 移除 install_requires 中的依赖项，统一到 requirements.txt 管理
      - 删除 console_scripts 入口声明，可能会影响命令行工具的使用

--- setup.py
+++ setup.py
@@ -4,18 +4,8 @@ setup(
     name="AuroCC",
     version="0.1",
     packages=find_packages(),
-    install_requires=[
-        'websockets',
-        'aiohttp',
-        'openai',
-    ],
     package_data={
         '': ['*.db', '*.yml'],
     },
     include_package_data=True,
-    entry_points={
-        'console_scripts': [
-            'aurocc=main:main',
-        ],
-    },
 )
