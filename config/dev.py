0716c253783af04c21adb168c53afb7d345a9648
AuroBreeze <1732373074@qq.com>
2025-05-19 11:14:57 +0800

    config(dev): 更新开发环境配置项
    - 添加内存存储路径和模型配置项
    - 为 QQ、API 和模型配置项添加中文注释
    - 优化代码格式和可读性

--- config/dev.py
+++ config/dev.py
@@ -1,13 +1,19 @@
 # QQ setttings for development environment
-
-QQ_BOT  = 'your_qq_bot_id' # QQ Bot ID
-
-QQ_ADMIN = 'your_qq_admin_id' # QQ Admin ID
+QQ_BOT  = 'your_qq_bot_id' # QQ Bot ID QQ机器人账号
+QQ_ADMIN = 'your_qq_admin_id' # QQ Admin ID QQ管理员账号
 
 
 # KEY settings for development environment
-
 DEEPSEEK_API_KEY = 'your_deepseek_api_key' # DeepSeek API Key
-
 AMAP_KEY = 'your_amap_key' # 高德API
+
+# memory_store_path settings for development environment
+
+MEMORY_STORE_PATH = './store/memory_store/' # Memory store path 聊天记录保存位置
+INDEX_STORE_PATH = './store/index/' # FAISS Index store path 向量数据库索引保存位置
+
+# model settings for development environment
+# FAISS 向量数据库emmbedding模型    
+MODEL_CHOOSE = 'distilbert-base-nli-stsb-mean-tokens' # Model choose 选择模型
+MODEL_STORE_PATH = './local_model/' # Model store path 模型保存位置
 
