4507355db3c5d1b6a264fb59d68ed5c162217dec
AuroBreeze <1732373074@qq.com>
2025-06-17 17:22:47 +0800

    build: 添加本地运行脚本
    - 新增 local_run.bat 文件，用于在本地环境中运行项目
    - 脚本包括激活虚拟环境、安装依赖、创建目录、迁移数据和启动主程序等步骤

---
+++ local_run.bat
@@ -0,0 +1,15 @@
+call .venv/Scripts/activate
+
+python -m pip install -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple/
+
+pip install .
+
+python utils/Create_dirs.py
+
+pip install .
+
+python utils/DataMigrator.py
+
+pip install .
+
+python main.py
